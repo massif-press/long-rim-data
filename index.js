5ec6e088724679d3b60a9a5a290ec90e85ae182f
OmnesPotens <21691862+OmnesPotens@users.noreply.github.com>
2022-09-11 20:47:04 -0400

    convert to commonjs module

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict"
+
+const data = {
+  actions: require('./lib/actions.json'),
+  frames: require('./lib/frames.json'),
+  lcp_manifest: require('./lib/lcp_manifest.json'),
+  mods: require('./lib/mods.json'),
+  systems: require('./lib/systems.json'),
+  talents: require('./lib/talents.json'),
+  weapons: require('./lib/weapons.json'),
+}
+
+module.exports = data;

98832a51cc3e82e82c826cc4dc62617397a96580
J. Arena <john.arena.3@gmail.com>
2026-04-11 12:16:37 -0500

    v3 release

--- scripts/build.js
+++ scripts/build.js
@@ -4,7 +4,7 @@ const info = require('../package.json');
 
 const name = info.name.split('/').pop();
 
-const filepath = `./dist/${name}-${info.version}.lcp`;
+const filepath = `./dist/long-rim-data.lcp`;
 
 zl.archiveFolder('./lib', filepath).then(
   function () {
